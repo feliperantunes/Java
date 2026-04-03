ded8248bfde4b408c994729c09f21c9f74d0e894
feliperantunes <felipe.rodriantunes@gmail.com>
2026-04-03 15:29:44 -0300

    Finalizando o exercicio do calculo de moda

--- Faculdade/src/Moda.java
+++ Faculdade/src/Moda.java
@@ -1,22 +1,24 @@
 public class Moda {
     public static void main(String[] args) {
-        int []vetor = {1, 5, 2, 3, 1, 3, 1};
+        int [] vetor = {1, 5, 2, 3, 1, 3, 1};
         int moda = 0;
         int quantidadeVezes = 0;
+        int recordeAtual = 0;
 
         for (int indice = 0; indice < vetor.length; indice++) {
-            for (int contador = 0; contador < vetor.length; contador++){
-                int teste1 = vetor[contador];
-                int teste2 = vetor[indice];
-                if (vetor [contador] == vetor[indice]){
+            int contador;
+            for (contador = 0; contador < vetor.length; contador++) {
+                if (vetor[contador] == vetor[indice]) {
                     quantidadeVezes++;
                 }
-                System.out.printf("%d | %d | %d\n", teste1, teste2,quantidadeVezes);
             }
+            if (quantidadeVezes > recordeAtual) {
+                recordeAtual = quantidadeVezes;
+                moda = vetor[indice];
+            }
+            quantidadeVezes = 0;
+            //System.out.printf("%d | %d | %d\n",vetor[indice],quantidadeVezes, recordeAtual);
         }
-
-        System.out.printf("Moda: %d\nQuantidade vezes: %d", moda, quantidadeVezes);
-
+        System.out.printf("Moda: %d", moda);
     }
-
 }
