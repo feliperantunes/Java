7438bfb720c2b8131536410884d39018d56c37f0
feliperantunes <felipe.rodriantunes@gmail.com>
2026-04-09 18:55:13 -0300

    Exibindo cada livro da lista

--- Faculdade/src/bibliotecaVirtual/inventarioLivros.java
+++ Faculdade/src/bibliotecaVirtual/inventarioLivros.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class inventarioLivros {
     public static void main(String[] args) {
-        LinkedList<Livro> minhaLista = new LinkedList();
+        LinkedList<Livro> minhaLista = new LinkedList<Livro>();
         Livro Duna = new Livro(
                 1,
                 "Duna: livro 1",
@@ -31,5 +31,13 @@ public class inventarioLivros {
         minhaLista.add(Duna);
         minhaLista.add(Dracula);
         minhaLista.add(Hamlet);
+
+        for (Livro livro : minhaLista) {
+            System.out.printf("Título do livro: %s\nAutor do livro: %s\nAno publicação: %d\nCategoria: %s\n-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n",
+                    livro.getTituloLivro(),
+                    livro.getAutorLivro(),
+                    livro.getAnoPublicacao(),
+                    livro.getCategoria());
+        }
     }
 }
