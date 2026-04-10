2c812412a92c8f4174ab0285e3c5bc69ce86ce8d
feliperantunes <felipe.rodriantunes@gmail.com>
2026-04-10 12:20:02 -0300

    Finalizando estrutura de pesquisa linear

--- Faculdade/src/atividades/PesquisaLinear.java
+++ Faculdade/src/atividades/PesquisaLinear.java
@@ -1,19 +1,29 @@
-package atividades;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-// Fazer algoritmo para busca linear o(n)
 public class PesquisaLinear {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int [] meuArray = {2, 3, 5, -1, 7, 9};
-        Arrays.sort(meuArray);
 
-        System.out.print("Insira o número que você queira buscar no array e iremos retornar a posição exata do mesmo: ");
-        int numemroDesejado = scanner.nextInt();
-        for (int c = 0; c < meuArray.length; c++) {
-            System.out.printf("%d | %d\n", c, meuArray[c]);
+        System.out.print("Insira um número que você queira buscar no array: ");
+        int numeroDesejado = scanner.nextInt();
+
+        int i;
+        int indiceDesejado = 0;
+        boolean flag = false;
+
+        for (i = 0; i < meuArray.length; i++) {
+            System.out.printf("%d | %d\n",i, meuArray[i]);
+            if (numeroDesejado == meuArray[i]){
+                flag = true;
+                indiceDesejado = i;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.printf("O número %d esta na posição %d da lista",numeroDesejado,(indiceDesejado+1));
+        } else {
+            System.out.println("Número não encontrado na lista");
         }
     }
 }
